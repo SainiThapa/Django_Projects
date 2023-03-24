6faebe3eb5ad63d526d9ed1184aa615bafaf88f2
Saini Thapa <magarprisu4747@gmail.com>
2023-03-24 16:31:54 +0545

    update commit

--- Proj1/urls.py
+++ Proj1/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('',include('app1.urls')),
+    path('',include('app1.urls'), name="home"),
+    path('add',include('app1.urls'),name='add'),
+    path('multiply',include('app1.urls'),name='multiply'),
     path('admin/', admin.site.urls),
 ]
