7c55162b5efb94b1d0c135846669d7843782c9da
Saini Thapa <magarprisu4747@gmail.com>
2023-03-29 23:08:17 +0545

    Dynamic Changes

--- Travello/models.py
+++ Travello/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Destination:
+     id : int
+     name : str
+     img : str
+     dest : str
+     price : int
--- Travello/views.py
+++ Travello/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Destination
 
 # Create your views here.
 
 def index(request):
-    return render(request,"index.html")
+    dest1=Destination()
+    dest1.name="Kathmandu"
+    dest1.description="Land of Dreams"
+    dest1.price="696"
+    return render(request,"index.html",{'dest1':dest1})
