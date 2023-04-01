633448fd93e67388965f5f0de43322ceb16458e8
Saini Thapa <magarprisu4747@gmail.com>
2023-04-01 22:40:13 +0545

    Login/Logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User,auth
-
+from typing import Union
+from django.contrib import messages
 # Create your views here.
 
 def register(request):
@@ -14,15 +15,37 @@ def register(request):
 
         if(password1==password2):
             if User.objects.filter(username=Username).exists():
-                print('Username already taken')
+                messages.info(request,"Username already taken")
+                return redirect('register')
             elif User.objects.filter(email=email).exists():
-                print('Email already in use')
+                messages.info(request,'Email already in use')
+                return redirect('register')
             else:
                 user=User.objects.create_user(username=Username,password=password1, email=email,first_name=first_name,last_name=last_name)
                 user.save();
                 print('USER CREATED !')
+                return redirect('login')
         else:
-            print("Password doesnot match")
-        return redirect('/')
+            messages.info(request,"Password doesnot match")
+            return redirect('register')
     else:
         return render(request,'register.html')
+
+def login(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+
+        user=auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.info(request,'Invalid Credentials')
+            return redirect('login')
+    else:
+        return render(request,'login.html')
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
